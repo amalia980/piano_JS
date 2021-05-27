a1fe876be9947c7c97a271a98466f1971903edfa
666chocolateprincess <amalia.p980@gmail.com>
2021-05-27 10:38:07 +0200

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-const getAllKeys = document.querySelectorAll('.key');
-
-
-
-function playSound(filename) {
+/*function playSound(filename) {
     var audio = new Audio('sounds/' + filename);
     audio.play();
 }
+*/
 
-playSound.addEventListener("click", getAllKeys);
+/*
+
+const getAllKeys = document.querySelectorAll('.key');
+
+function pressedKeys (event) {
+    const element = event.target;
+    const keyName = element.getAttribnute("data-value");
+
+    playSound(keyName + '.wav');
+}
+
+getAllKeys.foreach(element => {
+    element.addEventListener("click", pressedKeys);
+});
+
+*/
+
+document.querySelectorAll(".key").forEach(
+    el => el.addEventListener("click",
+        e => new Audio('sounds/' + e.target.getAttribute("data-value") + '.wav').play()
+    )
+);
